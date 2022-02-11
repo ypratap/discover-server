b0730537806b9406d7644c9f065b52483f429438
pratapy <pratapyalamanchi.1@gmail.com>
2022-02-10 21:42:42 -0500

    Initial commit

---
+++ src/test/java/io/shiprock/discoverserver/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shiprock.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
